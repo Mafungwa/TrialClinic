c54ff915d5ffb7af85819b1268e93ac56c69fe79
Kholekile2 <5100146@mypillar7uwc.ac.za>
2024-06-04 14:51:07 +0200

    Added Participant class

--- TrialClinic/Services/TrialLocalDatabase.cs
+++ TrialClinic/Services/TrialLocalDatabase.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CloudKit;
-using SQLite;
+﻿using SQLite;
 using TrialClinic.Models;
-using TrialClinic.Pages;
 using Location = TrialClinic.Models.Location;
 
 namespace TrialClinic.Services
